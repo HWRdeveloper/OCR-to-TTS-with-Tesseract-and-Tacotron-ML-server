108edb4ab1a106a83bb3b5bced7cc2e8630806a9
HWRdeveloper <ruehyeon12@gmail.com>
2021-07-07 14:07:55 +0900

    commit

--- app/src/main/java/hwr/demo/tesseract/android/MainActivity.java
+++ app/src/main/java/hwr/demo/tesseract/android/MainActivity.java
@@ -47,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
     private Uri mImageCaptureUri;
     Button button;
     ImageView imageView;
-
     TextView textView;
     String dataPath = "";
     @Override
